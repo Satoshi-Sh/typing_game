139ad80e6bd002717cbb5c5fb512d5e982858748
satoshi-sh <satoss1108@gmail.com>
2022-12-18 17:06:57 -0600

    Add underline to see mistakes for spaces

--- src/pages/game/Game.jsx
+++ src/pages/game/Game.jsx
@@ -17,11 +17,21 @@ export default function Game(){
     const [cursor,setCursor] = useState(0)
     const [mistypes,setMistypes] = useState(0)
     
+    function isRed(){
+        let isR = false;
+        const spans = document.querySelectorAll('span')
+        for (let span of spans){
+        if (span.style.color=='red'){
+            isR =true
+        } 
+        }
+        return isR
+    }
 
     useEffect(() => {
         function handleKeyDown(e) {
             let letter = document.querySelector(`#letter${cursor}`)
-            console.log(text[cursor], e.key)
+
             if (e.key =="Shift") return;
             else if (e.key=="Backspace"){
                 if (cursor > 0){
@@ -29,12 +39,26 @@ export default function Game(){
                 letter = document.querySelector(`#letter${cursor-1}`)
                 letter.style.color='rgba(128, 128, 128, 0.342)'   
             }
-            else if (e.key==text[cursor]){
-                console.log('Correct')
+            else if (e.key=='Space'){
                 letter.style.color='green'
                 if (cursor < text.length){
                 setCursor(cursor+1)
-                if (cursor==text.length-1){
+                if (cursor==text.length-1 && !isRed()){
+
+                    const time = document.querySelector('#time').innerText
+                    const wpm =  Math.round((word_counts/parseInt(time)) * 60)
+                    navigate('/score', {state:{mode:location.state.level,time:time,mistypes,wpm}})
+                    
+                }
+            }
+
+            }
+            else if (e.key==text[cursor]){
+                letter.style.color='green'
+                if (cursor < text.length){
+                setCursor(cursor+1)
+                if (cursor==text.length-1 && !isRed()){
+
                     const time = document.querySelector('#time').innerText
                     const wpm =  Math.round((word_counts/parseInt(time)) * 60)
                     navigate('/score', {state:{mode:location.state.level,time:time,mistypes,wpm}})
@@ -44,13 +68,13 @@ export default function Game(){
                 
             }
             else {   
-                console.log('wrong')
                 letter.style.color='red'
+                letter.style.textDecoration='underline'
+                
                 setMistypes(mistypes+1)
-                if (cursor <text.length){
+                if (cursor <text.length-1){
                 
                 setCursor(cursor+1)}
-
             }
         }
     
